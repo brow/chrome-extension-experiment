79e4aee24394912348ff09f1f601d8eba14fb513
Tom Brow <tom@Toms-MacBook-Pro.local>
2022-08-07 22:12:51 -0500

    insert UI into PR page

--- content.js
+++ content.js
@@ -1,7 +1,23 @@
-const login = document.head.querySelector('meta[name=user-login]')?.content ?? null;
+const login = document.head.querySelector('meta[name=user-login]')?.content ?? null
 
 if (login) {
+  const component = document.createElement('div')
+  component.innerText = login
+
+  const addComponent = () => {
+    if (!component.isConnected) {
+      document.querySelector('#partial-discussion-header')?.insertAdjacentElement('beforebegin', component)
+    }
+  }
+
+  // const frame = document.querySelector('turbo-frame#repo-content-turbo-frame')
+  let observer = new MutationObserver((mutations) => {
+    addComponent()
+  })
+  observer.observe(document.querySelector('body'), {childList: true, subtree: true})
+
+
   chrome.runtime.sendMessage({login: login}, (response) => {
-    alert(JSON.stringify(response ?? runtime.lastError, null, 2));
-  });
+    // alert(JSON.stringify(response ?? runtime.lastError, null, 2))
+  })
 }
